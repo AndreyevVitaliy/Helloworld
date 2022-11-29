f041aae80bb1e755f314be4681fd33b9f70821cb
vitaliy <andv@kvarta.kz>
2022-11-29 23:31:31 +0100

    1.01

--- src/Lesson19.java
+++ src/Lesson19.java
@@ -1,13 +1,31 @@
 public class Lesson19 {
     public static void main(String[] args) {
         Human human = new Human();
-        human.setAge(18);
-        human.setName("Tom");
         human.getInfo();
     }
 }
 
 class Human{
+    /**
+     * Конструкторов класс может быть несколько
+     * В данном случае отражен пример когда объект может быть создан со
+     * значениями по умолчанию или с со значениями, которые указал пользователь
+     */
+    public Human(){ //Конструктор класса
+        this.name = "Новое имя";
+        this.age = 0;
+    }
+
+    public Human(String name){ //Конструктор класса
+        this.name = name;
+        this.age = 0;
+    }
+
+    public Human(String name, int age){ //Конструктор класса
+        this.name = name;
+        this.age = age;
+    }
+
     private String name;
     private int age;
 
